7c1ace838e95951972ba2aee7bb5ce0751dc012f
Deepak_Baskar <91734673+Deepakbaskar94@users.noreply.github.com>
2022-01-25 17:47:24 +0530

    Add files via upload

---
+++ portscan/netscanlive.py
@@ -0,0 +1,34 @@
+#https://www.tutorialspoint.com/python_penetration_testing/python_penetration_testing_network_scanner.htm
+#port scanner using TCP - looks for the port 135 to find is it live or not
+import socket
+from datetime import datetime
+net = input("Enter the IP address: ")
+net1 = net.split('.')
+print(net1)
+a = '.'
+
+net2 = net1[0] + a + net1[1] + a + net1[2] + a
+st1 = int(input("Enter the Starting Number: "))
+en1 = int(input("Enter the Last Number: "))
+en1 = en1 + 1
+t1 = datetime.now()
+
+def scan(addr):
+   s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+   socket.setdefaulttimeout(1)
+   result = s.connect_ex((addr,22))
+   if result == 0:
+      return 1
+   else :
+      return 0
+
+def run1():
+   for ip in range(st1,en1):
+      addr = net2 + str(ip)
+      if (scan(addr)):
+         print (addr , "is live")
+         
+run1()
+t2 = datetime.now()
+total = t2 - t1
+print ("Scanning completed in: " , total)
